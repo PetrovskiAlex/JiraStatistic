a05e0447dfd5607ec660c993e6ff148db0390abb
Alex Petrovski <a.petrovsky@outlook.com>
2021-01-12 06:57:17 +0200

    init project

---
+++ JiraStatistic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JiraStatistic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
