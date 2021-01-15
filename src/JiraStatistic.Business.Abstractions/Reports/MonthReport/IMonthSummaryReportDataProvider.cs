45820c6a3ad52159288035ad254e96afb36b610e
Alex Petrovski <petrovski.alex.v@gmail.com>
2021-01-15 10:59:38 +0200

    stub month report service

---
+++ src/JiraStatistic.Business.Abstractions/Reports/MonthReport/IMonthSummaryReportDataProvider.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using JiraStatistic.Domain.Settings.Jira;
+
+namespace JiraStatistic.Business.Abstractions.Reports.MonthReport
+{
+    public interface IMonthSummaryReportDataProvider
+    {
+        Task<MonthSummaryReportData> GetData(JiraAuthSettings? authSettings = null);
+    }
+}
---
+++ src/JiraStatistic.Business/Reports/MonthReport/MonthSummaryReport.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using JiraStatistic.Business.Abstractions.Reports.MonthReport;
+using JiraStatistic.Domain.Settings.Report;
+using JiraStatistic.JiraClient.Clients.Search;
+
+namespace JiraStatistic.Business.Reports.MonthReport
+{
+    public class MonthSummaryReport : IMonthSummaryReport
+    {
+        private readonly IJiraSearchClient _jiraSearchClient;
+
+        public MonthSummaryReport(IJiraSearchClient jiraSearchClient)
+        {
+            _jiraSearchClient = jiraSearchClient;
+        }
+        
+        public Task MakeReport(MonthSummarySettings settings)
+        {
+            //GetData
+            //Make a report
+            //Save report
+
+            throw new System.NotImplementedException();
+        }
+    }
+}
